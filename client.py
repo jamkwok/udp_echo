1c91fafcf632d74089065b0e506186d6026b894e
James Kwok <james.kwok@droneshield.com>
2021-11-01 20:42:47 +1100

    update

--- client.py
+++ client.py
@@ -2,11 +2,11 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = '0.0.0.0'
+server_address = '10.147.18.58'
 server_port = 31337
 client_socket.connect((server_address, server_port))
 
 message = 'Hello World'
-client_socket.send(message.encode())
-response = client_socket.recv(1024).decode()
-print("Test passed" if message == response else "Test failed")
+client_socket.sendto(message.encode(), ("127.0.0.1",31337))
+response = client_socket.recvfrom(1024)
+print(response[0].decode())
