d31500d7fca05da3daab7bc73ece600c6b939525
James Kwok <james.kwok@droneshield.com>
2021-11-02 15:27:15 +1100

    use localhost

--- client.py
+++ client.py
@@ -2,11 +2,10 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = '10.147.18.58'
+server_address = '127.0.0.1'
 server_port = 31337
-client_socket.connect((server_address, server_port))
 
 message = 'Hello World'
-client_socket.sendto(message.encode(), ("127.0.0.1",31337))
+client_socket.sendto(message.encode(), (server_address,server_port))
 response = client_socket.recvfrom(1024)
 print(response[0].decode())
